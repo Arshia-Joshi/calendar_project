98efd3cdc4745fbee6369256bdf618164f870105
arshia-backup <160587953+arshia-backup@users.noreply.github.com>
2024-04-05 00:05:15 +0530

    dates

--- main.c
+++ main.c
@@ -1,20 +1,14 @@
- #include<stdio.h>
- void swap(int*a,int*b,int x,int y)
- {
- 
-  int temp= *a;
-  *a=*b;
-  *b=temp;
- 
-  int temp1= x;
-  x=y;
-  y=temp1;
-  printf("%d\n%d\n",x,y);
- }
- int main()
- {
-  int a=10, b=20, x=30, y=40;
-swap(&a,&b,x,y);
-printf("%d\n%d\n%d\n%d\n",a,b,x,y);
+#include<stdio.h>
+int main()
+{
+    int i, days;
+    printf("***** CALENDAR *****\n");
+    printf("MON   TUE   WED   THU   FRI   SAT   SUN\n");
+    for(i=1;i<31;i++)
+    {
+        printf("%d\n",i);
+    }
+   
 return 0;
- }
+}
+
