ea1a93b5d4ca7c6942adbd5be28a0dd796bc1369
arshia-backup <160587953+arshia-backup@users.noreply.github.com>
2024-04-06 15:30:45 +0530

    basic

--- main.c
+++ main.c
@@ -1,14 +1,29 @@
-#include<stdio.h>
-int main()
-{
-    int i, days;
-    printf("***** CALENDAR *****\n");
-    printf("MON   TUE   WED   THU   FRI   SAT   SUN\n");
-    for(i=1;i<31;i++)
-    {
-        printf("%d\n",i);
-    }
-   
-return 0;
-}
+#include <stdio.h>
 
+int main() {
+    int i, day;
+    int days_in_month = 30; // April has 30 days
+    int starting_day = 6;   // April 1st, 2024 is a Saturday (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
+
+    printf("  April 2024\n");
+    printf(" Su Mo Tu We Th Fr Sa\n");
+
+    // Print leading spaces if the month doesn't start on a Sunday
+    for (i = 0; i < starting_day; i++) {
+        printf("   ");
+    }
+
+    // Print the days of the month
+    for (day = 1; day <= days_in_month; day++) {
+        printf("%3d", day);
+        starting_day++;
+        // If it's Saturday, start a new line
+        if (starting_day % 7 == 0) {
+            printf("\n");
+        }
+    }
+
+    printf("\n");
+
+    return 0;
+}
